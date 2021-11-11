62748f0a93c55af836eb3436970a5e0ad6815505
Jonggun Park <ooostus@gmail.com>
2021-11-12 01:00:58 +0900

    second commit - DBMS connect

---
+++ src/main/java/com/project/persistence/BoardDAOImpl.java
@@ -0,0 +1,38 @@
+package com.project.persistence;
+
+import java.util.List;
+import com.project.domain.BoardVO;
+import javax.inject.Inject;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+public class BoardDAOImpl implements BoardDAO {
+	@Inject
+	private SqlSession session;
+	private static String namespace = "com.myp.mapper.BoardMapper";
+
+	@Override
+	public void create(BoardVO vo) throws Exception {
+		session.insert(namespace+".create", vo);
+	}
+
+	@Override
+	public BoardVO read(Integer bno) throws Exception {
+		return session.selectOne(namespace + ".read", bno);
+	}
+
+	@Override
+	public void update(BoardVO vo) throws Exception {
+		session.update(namespace+".update", vo);
+	}
+
+	@Override
+	public void delete(Integer bno) throws Exception {
+		session.delete(namespace+".delete", bno);
+	}
+	
+	@Override
+	public List<BoardVO> listAll() throws Exception {
+		return session.selectList(namespace + ".listAll");
+	}
+}
